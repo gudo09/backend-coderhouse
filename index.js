1685f78ced1b0e90999adde351765b2b81ac1326
Franco Gudiño <francogudino12@gmail.com>
2024-04-01 16:31:13 -0300

    compilo typescript

--- index.js
+++ index.js
@@ -1,54 +1,66 @@
-"use strict";
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
+};
 //creo la clase ProductManager que es la que va a crear instancias
-class ProductManager {
+var ProductManager = /** @class */ (function () {
     //la llamada al constuctor genera un array vacio
-    constructor() {
+    function ProductManager() {
         this.id = 1;
         this.products = [];
     }
     //creo el metodo addProduct que va a recibir un elemento del tipo Product y lo agrega al arreglo Products[]
     //el metodo es de tipo void porque no retorna nada
-    addProduct(product) {
+    ProductManager.prototype.addProduct = function (product) {
         //le agrego un id al nuevo producto
-        const newProductWithId = Object.assign({ id: this.id }, product);
+        var newProductWithId = __assign({ id: this.id }, product);
         //Valido si la propiedad code se repite en algun otro producto
-        const duplicatedCode = this.products.some((product) => product.code === newProductWithId.code);
+        var duplicatedCode = this.products.some(function (product) { return product.code === newProductWithId.code; });
         //Si está diplicado muestro un mensaje por consola
         if (duplicatedCode) {
-            console.log(`\nEl código ${newProductWithId.code} ya existe en otro producto, no se pudo agregar el producto con el título '${newProductWithId.title}'`);
+            console.log("\nEl c\u00F3digo ".concat(newProductWithId.code, " ya existe en otro producto, no se pudo agregar el producto con el t\u00EDtulo '").concat(newProductWithId.title, "'"));
             return;
         }
         this.products.push(newProductWithId);
         //aumento el id para el siguiente producto
         this.id++;
-    }
+    };
     //el metodo es de tipo ProductWithId[] porque retorna un arreglo con los productos y su id
-    getProducts() {
-        let result = "\n---------------------------\nListado de productos:\n---------------------------\n";
+    ProductManager.prototype.getProducts = function () {
+        var _this = this;
+        var result = "\n---------------------------\nListado de productos:\n---------------------------\n";
         //recorro el array de productos y ejecuto el toString para cada producto
-        this.products.forEach((prod) => {
-            result += this.toString(prod);
+        this.products.forEach(function (prod) {
+            result += _this.toString(prod);
         });
         return result + "\n---------------------------";
-    }
+    };
     //el metodo es de tipo ProductWithId[] porque retorna un arreglo con los productos y su id
-    getProductById(id) {
-        const result = this.products.find((prod) => id === prod.id);
+    ProductManager.prototype.getProductById = function (id) {
+        var result = this.products.find(function (prod) { return id === prod.id; });
         if (result === undefined)
-            return `\nNo se ha encontrado el producto con el ID ${id}`;
-        return `\nSe ha encontrado un producto con el id ${id}:\n${this.toString(result)}`;
-    }
+            return "\nNo se ha encontrado el producto con el ID ".concat(id);
+        return "\nSe ha encontrado un producto con el id ".concat(id, ":\n").concat(this.toString(result));
+    };
     //metodo toString para imprimir cada producto
-    toString(prod) {
-        let result = "\n";
-        for (let propiedad in prod) {
-            result += `${propiedad}: ${prod[propiedad]}\n`;
+    ProductManager.prototype.toString = function (prod) {
+        var result = "\n";
+        for (var propiedad in prod) {
+            result += "".concat(propiedad, ": ").concat(prod[propiedad], "\n");
         }
         return result;
-    }
-}
-//probando el codigo
-const listadoProductos = new ProductManager();
+    };
+    return ProductManager;
+}());
+//------------------------------------------------------probando el codigo
+var listadoProductos = new ProductManager();
 listadoProductos.addProduct({
     title: "Producto 1",
     description: "Este es el primer producto",
