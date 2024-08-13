e0c1201e395623332b9d6919450486b74929af00
Franco Gudiño <francogudino12@gmail.com>
2024-08-12 21:47:19 -0300

    Clusters no funciona con Singleton

--- src/app.ts
+++ src/app.ts
@@ -21,7 +21,6 @@ import TicketsCustomRouter from "./routes/ticketsCustom.routes.js";
 import AuthCustomRouter from "./routes/authCustom.routes.js";
 import LoggingCustomRouter from "./routes/loggingCustom.routes.js";
 
-/*
 import cluster from "cluster";
 import { cpus } from "os";
 if (cluster.isPrimary) {
@@ -32,78 +31,74 @@ if (cluster.isPrimary) {
   cluster.on("exit", (worker, code, signal) => {
     console.log(`Se cayó la instancia${worker.process.pid}`);
     cluster.fork();
-
   });
   cluster.on("error", (err, worker) => {
     console.error(`Worker ${worker.process.pid} error: ${err.message}`);
   });
 } else {
- 
-}
-*/
+  try {
+    //Creo un a instancia del servidor de express, determino el puerto donde va a iniciar y una instancia del ProductManager
+    const app = express();
 
-try {
-  //Creo un a instancia del servidor de express, determino el puerto donde va a iniciar y una instancia del ProductManager
-  const app = express();
+    //ruta para archivos estaticos
+    app.use("/static", express.static(`${config.DIRNAME}/public`));
 
-  //ruta para archivos estaticos
-  app.use("/static", express.static(`${config.DIRNAME}/public`));
+    const expressInstance = app.listen(config.PORT, async () => {
+      // conecto mongoose con la base de datos en Atlas
+      //await mongoose.connect(config.MONGOBD_URI);
+      MongoSingleton.getInstance();
 
-  const expressInstance = app.listen(config.PORT, async () => {
-    // conecto mongoose con la base de datos en Atlas
-    //await mongoose.connect(config.MONGOBD_URI);
-    MongoSingleton.getInstance();
+      //inicializo el server de socket.io desde el archivo socket.js
+      const socketServer = initSocket(expressInstance);
+      app.set("socketServer", socketServer);
 
-    //inicializo el server de socket.io desde el archivo socket.js
-    const socketServer = initSocket(expressInstance);
-    app.set("socketServer", socketServer);
+      //configuraciones de express
+      app.use(express.json());
+      app.use(express.urlencoded({ extended: true }));
 
-    //configuraciones de express
-    app.use(express.json());
-    app.use(express.urlencoded({ extended: true }));
+      //configuraciones de cors
+      app.use(cors({ origin: "*", methods: ["GET", "POST", "PUT", "DELETE"] }));
 
-    //configuraciones de cors
-    app.use(cors({ origin: "*", methods: ["GET", "POST", "PUT", "DELETE"] }));
+      // configuraciones de session
+      app.use(
+        session({
+          store: MongoStore.create({ mongoUrl: config.MONGOBD_URI, ttl: 300 }),
+          secret: config.SECRET,
+          resave: true,
+          saveUninitialized: true,
+        })
+      );
+      app.use(passport.initialize());
+      app.use(passport.session());
+      app.use(cookieParser(config.SECRET));
 
-    // configuraciones de session
-    app.use(
-      session({
-        store: MongoStore.create({ mongoUrl: config.MONGOBD_URI, ttl: 300 }),
-        secret: config.SECRET,
-        resave: true,
-        saveUninitialized: true,
-      })
-    );
-    app.use(passport.initialize());
-    app.use(passport.session());
-    app.use(cookieParser(config.SECRET));
+      // configuraciones Handlebars
+      app.engine("handlebars", handlebars.engine());
+      app.set("views", `${config.DIRNAME}/views`);
+      app.set("view engine", "handlebars");
 
-    // configuraciones Handlebars
-    app.engine("handlebars", handlebars.engine());
-    app.set("views", `${config.DIRNAME}/views`);
-    app.set("view engine", "handlebars");
+      // uso el Logger para registros
+      app.use(addLogger);
 
-    // uso el Logger para registros
-    app.use(addLogger);
+      // hago uso de las rutas
+      // Instancio un objeto de TestCustomRouter
+      // y llamo al getRouter para que me devuelva un tipo express.Router
+      app.use("/api/test", new TestCustomRouter().getRouter());
+      app.use("/", new ViewsCustomRouter().getRouter());
+      app.use("/api/products", new ProductsCustomRouter().getRouter());
+      app.use("/api/users", new UsersCustomRouter().getRouter());
+      app.use("/api/carts", new CartsCustomRouter().getRouter());
+      app.use("/api/sessions", new AuthCustomRouter().getRouter());
+      app.use("/api/tickets", new TicketsCustomRouter().getRouter());
+      app.use("/api/log", new LoggingCustomRouter().getRouter());
 
-    // hago uso de las rutas
-    // Instancio un objeto de TestCustomRouter
-    // y llamo al getRouter para que me devuelva un tipo express.Router
-    app.use("/api/test", new TestCustomRouter().getRouter());
-    app.use("/", new ViewsCustomRouter().getRouter());
-    app.use("/api/products", new ProductsCustomRouter().getRouter());
-    app.use("/api/users", new UsersCustomRouter().getRouter());
-    app.use("/api/carts", new CartsCustomRouter().getRouter());
-    app.use("/api/sessions", new AuthCustomRouter().getRouter());
-    app.use("/api/tickets", new TicketsCustomRouter().getRouter());
-    app.use("/api/log", new LoggingCustomRouter().getRouter());
+      app.use(errorsHandler);
 
-    app.use(errorsHandler);
-
-    console.log(`Servidor iniciado en el puerto ${config.PORT} (PID ${process.pid})`);
-    console.log(`Ruta raíz: ${config.DIRNAME}`);
-    console.log(`Puedes acceder desde ${config.BASE_URL}/login`);
-  });
-} catch (err) {
-  console.log((err as Error).message);
+      console.log(`Servidor iniciado en el puerto ${config.PORT} (PID ${process.pid})`);
+      console.log(`Ruta raíz: ${config.DIRNAME}`);
+      console.log(`Puedes acceder desde ${config.BASE_URL}/login`);
+    });
+  } catch (err) {
+    console.log((err as Error).message);
+  }
 }
